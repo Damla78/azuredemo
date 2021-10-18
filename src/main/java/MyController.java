12e821035ad859f45ab2f0d4ce4de4d24ef1c2bd
Rabia Karapinar <rabia.karapinar@rabobank.nl>
2021-10-18 19:12:38 +0200

    fix path in controller

---
+++ src/main/java/MyController.java
@@ -0,0 +1,12 @@
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class MyController {
+
+    @GetMapping("/")
+    public String greeting(Model model) {
+        return "index";
+    }
+}
